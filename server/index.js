25dd33ff3f7e68df1a72f9850b72eb05c1280c5b
kmorgport <kmorgan0057@gmail.com>
2021-11-19 09:51:32 -0600

    file structure

--- server/index.js
+++ server/index.js
@@ -1,3 +1,18 @@
-import express from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
 
 const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true,  useUnifiedTopology: true })
+    .then(()=> app.listen(PORT, ()=> console.log(`Server is running on Port: ${PORT}`)))
+    .catch( error => console.log(error.message));
