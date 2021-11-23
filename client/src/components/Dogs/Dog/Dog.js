ffd8eb9f1d9aec8b793a86465c925d5463e075a1
kmorgport <kmorgan0057@gmail.com>
2021-11-23 09:51:10 -0600

    rendering Dog lists

--- client/src/components/Dogs/Dog/Dog.js
+++ client/src/components/Dogs/Dog/Dog.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Card, CardContent, CardMedia, Typography} from '@material-ui/core'
+import useStyles  from './styles';
 
-const Dog = ()=>{
+const Dog = ({dog})=>{
+    const classes = useStyles();
+    
     return (
-        <h1>Dog</h1>
+        <Card className={classes.card} raised elevation={6}>
+            <CardMedia className={classes.cardAction} title={dog.name}/>
+            <div className={classes.overlay}>
+                <Typography variant="h6">{dog.name}</Typography>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{dog.breeds.map((breed)=> `${breed} `)}</Typography>
+            </div>
+            <CardContent>
+                <Typography className={classes.title} variant="h5" gutterBottom>{dog.message}</Typography>
+            </CardContent>
+        </Card>
     )
 }
 
--- client/src/components/Dogs/Dogs.js
+++ client/src/components/Dogs/Dogs.js
@@ -2,16 +2,23 @@ import React from 'react';
 import Dog from './Dog/Dog';
 import useStyles from './styles';
 import { useSelector } from 'react-redux';
+import { Grid } from "@material-ui/core"
 
 const Dogs = ()=>{
     const dogs = useSelector((state)=> state.dogs);
-    console.log(dogs);
     const classes = useStyles();
+    if(!dogs.length) return "No Posts"
     return(
-        <>
-            <h1 className={classes.mainContainer}>Dogs</h1>
-            <Dog></Dog>
-        </>
+        <Grid className={classes.container} container alignItems="stretch" spacing="3">
+            {
+                dogs.map((dog)=>(
+                    <Grid key={dog._id} item xs={12} sm={6} lg={3}>
+                        <Dog dog={dog}/>
+                    </Grid>
+                ))
+            }
+
+        </Grid>
     )
 }
 
