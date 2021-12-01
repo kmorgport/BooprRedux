b1c7bc3a2352a1227ae79465e04bf3ae1b5e3ca3
kmorgport <kmorgan0057@gmail.com>
2021-12-01 16:59:53 -0600

    Dog component refactor

--- client/src/components/Dogs/Dog/Dog.js
+++ client/src/components/Dogs/Dog/Dog.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Card, CardContent, CardMedia, Typography} from '@material-ui/core'
 import useStyles  from './styles';
 import { useDispatch } from 'react-redux';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import { useNavigate } from 'react-router-dom';
 import { boopDog, deleteDog } from '../../../actions/dogs';
 
 
-const Dog = ({dog})=>{
+const Dog = ({dog, setCurrentId})=>{
     const user = JSON.parse(localStorage.getItem('profile'));
     const [boops, setBoops] = useState(dog?.boops);
     const dispatch = useDispatch();
@@ -39,19 +40,56 @@ const Dog = ({dog})=>{
     
         return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
       };
+
+    const openPost = ()=>{
+
+    }
     
     return (
         <Card className={classes.card} raised elevation={6}>
-            <CardMedia className={classes.media} title={dog.name}/>
-            <div className={classes.overlay}>
-                <Typography variant="h6">{dog.name}</Typography>
-            </div>
-            <div className={classes.details}>
-                <Typography variant="body2" color="textSecondary">{dog.breeds.map((breed)=> `${breed} `)}</Typography>
-            </div>
-            <CardContent>
-                <Typography className={classes.title} variant="h5" gutterBottom>{dog.bio}</Typography>
-            </CardContent>
+            <ButtonBase
+                component="span"
+                name="test"
+                className={classes.cardAction}
+                onClick={openPost}
+            >
+                <CardMedia className={classes.media} image={dog.pictures[0] || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={dog.name}/>
+                <div className={classes.overlay}>
+                    <Typography variant="h6">{dog.name}</Typography>
+                    <Typography variant="body2">{moment(dog.dateAdded).fromNow()}</Typography>
+                </div>
+                {(user?.result?.googleId === dog?.owner || user?.result?._id === dog?.owner)&&(
+                    <div className={classes.overlay2} name="edit">
+                        <Button
+                        onClick={ e => {
+                            e.stopPropagation();
+                            setCurrentId(dog._id)
+                        }}
+                        style={{color: 'white'}}
+                        size="small"
+                        >
+                            <MoreHorizIcon fontSize="default"/>
+                        </Button>
+                    </div>
+                )}
+                <div className={classes.details}>
+                        <Typography variant="body2" color="testSecondary" component="h2">Placeholders for pup pack</Typography>
+                </div>
+                <Typography className={classes.title} gutterBottom variant="h5" comment="h2">{dog.name}</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">{dog.bio.split(' ').splice(0,20).join(' ')}</Typography>
+                </CardContent>
+            </ButtonBase>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" disabled={!user?.result} onClick={handleBoop}>
+                    <Boops/>
+                </Button>
+                {(user?.result?.googleId === dog?.creator || user?.result?._id === dog?.creator)&&(
+                    <Button size="small" color="secondary" onClick={()=> dispatch(deleteDog(dog._id))}>
+                        <DeleteIcon fontSize="small"/>&nbsp; Delete
+                    </Button>
+                )}
+            </CardActions>
         </Card>
     )
 }
