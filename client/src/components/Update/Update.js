eb787f17da67264d9566ab002356c91d61754586
kmorgport <kmorgan0057@gmail.com>
2021-12-12 13:56:58 -0600

    styling for edit component

--- client/src/components/Update/Update.js
+++ client/src/components/Update/Update.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Container, Grow, Grid, AppBar, TextField, Button, Typography} from '@material-ui/core';
+import useStyles from './styles';
+import Filebase from 'react-file-base64'
 
 const Update = ()=>{
+    const classes = useStyles();
     return (
         <>
-        <Grid container align-items="center">
-            <Typography variant="h2">My Dogs</Typography>
+        <Grid container alignItems="center" row justifyContent='center'>
+            <Typography variant="h4" margintTop="4">Dog Editor</Typography>
+            <Grid container justifyContent='center' m="1" className={classes.borders}>
+                <Grid item alignItems="center">
+                    <Typography>Photo Edit</Typography>
+                </Grid>
+            </Grid>
+            <Grid container justifyContent='center' m="1"className={classes.borders}>
+                <Grid item alignItems="center">
+                    <Typography>Edit Info</Typography>
+                </Grid>
+            </Grid>
         </Grid>
         </>
     )
