17947f4f297861a410a8804522dc5a6619e63066
kmorgport <kmorgan0057@gmail.com>
2021-12-15 21:52:37 -0600

    buildig out update form

--- client/src/components/Update/Update.js
+++ client/src/components/Update/Update.js
@@ -6,6 +6,9 @@ import image from '../../img/Logo.png'
 
 const Update = ()=>{
     const classes = useStyles();
+    const handleSubmit = ()=>{
+
+    }
     return (
         <>
         <Grid container alignItems="center" direction="column" justifyContent='center'>
@@ -40,6 +43,38 @@ const Update = ()=>{
             <Grid container justifyContent='center' m="1"className={classes.borders}>
                 <Grid item alignItems="center">
                     <Typography>Edit Info</Typography>
+                    <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+                        <TextField name="name" variant="outlined" label="Name" fullWidth value={dogData.name} onChange={onChangeNameHandler}/>
+                        <FormControl fullWidth sx={{m:1, width: 300}}>
+                        <InputLabel id="multiple-name-label">Breed</InputLabel>
+                        <Select
+                            labelId= "multiple-breed-label"
+                            id="multiple-breeds"
+                            multiple
+                            value={breedName}
+                            onChange={onChangeBreedsHandler}
+                            onBlur={setBreedsHandler}
+                            input={<OutlinedInput label="Breed"/>}
+                        >
+                            {breeds.map((breed)=>(
+                                <MenuItem
+                                    key={breed._id}
+                                    value={breed.breed}
+                                >
+                                    {breed.breed}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <TextField name="bio" variant="outlined" label="Bio" fullWidth value={dogData.bio} onChange={onChangeBioHandler}/>
+                    <FormControl component="fieldset">
+                        <FormLabel component="legend">Sex</FormLabel>
+                        <RadioGroup row aria-label="sex" name="sex" value={dogData.sex} onChange={onChangeSexHandler}>
+                            <FormControlLabel value="female" control={<Radio/>} label="Female"/>
+                            <FormControlLabel value="male" control={<Radio/>} label="Male"/>
+                        </RadioGroup>
+                    </FormControl>
+                    </form>
                 </Grid>
             </Grid>
         </Grid>
